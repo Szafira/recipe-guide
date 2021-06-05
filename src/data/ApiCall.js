4e7c03f328a09409d2ddfa5b81cd156a3064be83
Klaudia Sawicka <sawicka.klaudi@gmail.com>
2021-06-05 14:47:48 +0200

    Thumbnail icon null fix

--- src/data/ApiCall.js
+++ src/data/ApiCall.js
@@ -17,21 +17,33 @@ console.log(fullUrl);
 request.open('GET', fullUrl, true)
 
 request.onload = function () {
-  // Begin accessing JSON data here
+  //Dostęp do API
   var data = this.responseText;
   var parsed = JSON.parse(data);
-  var jsonToString = JSON.stringify(parsed.results)
+  //Dodawanie domyślnej ikonki dla przepisu
+  var defaultLink= "//http://img.recipepuppy.com/10.jpg";
+  var parsedThumbnailsCheck = parsed.results.map(function(results, thumbnail)
+  {
+    if(parsed.results.thumbnail ==' '){ 
+    }else {
+      (thumbnail, Object.assign(results,  {thumbnail: defaultLink} )); 
+    }
+  });
+  //Dodawanie ID do danego wyniku wyszukiwania
+  var parsedThumbnailsCheckWithIndex =parsed.results.map((results, id) => Object.assign(results, { id }))
+  console.log(parsed.results)
 
-  console.log(parsed.results);
+  var jsonToString = JSON.stringify(parsedThumbnailsCheckWithIndex)
+
 
 //Zapisanie do pliku
- var readyToFile = "export interface Result {\ntitle: string;\nthumbnail: string;\ningredients: string;\nhref: string;\n} \nconst results: Result[] = \n"+ jsonToString + "\n\nexport const getResults = () => results;";
+  var readyToFile = "export interface Result {\ntitle: string;\nthumbnail: string;\ningredients: string;\nhref: string;\n} \nconst results: Result[] = \n"+ jsonToString + "\n\nexport const getResults = () => results;";
 
 fs.writeFile('ApiResults.ts', readyToFile, (err) => {
     if (err) {
         throw err;
     }
-    console.log("JSON data is saved.");
+
 
 }); 
 };
